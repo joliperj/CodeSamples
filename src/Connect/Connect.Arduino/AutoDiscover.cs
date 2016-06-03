040ba8c86822417058349488db106158e2b6b8ba
Steven Ryssaert <steven@uwconcept.be>
2016-06-03 15:09:52 +0200

    New Connect library name

---
+++ src/Connect/Connect.Arduino/AutoDiscover.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+
+namespace Connect.Arduino
+{
+    public static class AutoDiscover
+    {
+        private static UsbSerial connection = null;
+        private static RemoteDevice arduino = null;
+
+        public static async Task<RemoteDevice> ConnectDevice()
+        {
+            await Task.Delay(1000);
+
+            if(connection == null)
+            {
+                var usbDevices = await UsbSerial.listAvailableDevicesAsync();
+                DeviceInformation info = null;
+
+                for (int i = 0; i < usbDevices.Count; i++)
+                {
+                    var props = usbDevices[i].Properties.Values.ToList();
+                    for (int y = 0; y < props.Count(); y++)
+                    {
+                        if (props[y] == null)
+                            continue;
+
+                        if (props[y].ToString().Contains("USB-SERIAL"))
+                        {
+                            info = usbDevices[i];
+                            break;
+                        }
+                    }
+
+                    if (info != null)
+                        break;
+                }
+
+                if (info == null)
+                    return null;
+
+                connection = new UsbSerial(info);
+                connection.begin(57600, SerialConfig.SERIAL_8N1);
+            }
+            
+            if(arduino == null)
+            {
+                arduino = new RemoteDevice(connection);
+            }
+
+            return arduino;
+        }
+    }
+}
